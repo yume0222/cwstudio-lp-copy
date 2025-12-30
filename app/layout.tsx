c168ffbac15081339f04023c79ae5c992404c2ae
yume0222 <rin_rin_lovebird@icloud.com>
2025-12-30 19:27:17 +0900

    修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
 import type { Metadata } from "next";
-import './globals.css';
+import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://cwstudio-lp-copy.vercel.app'),
+  metadataBase: new URL("https://cwstudio-lp-copy.vercel.app"),
   title: {
-    template: '%s | 小説の執筆ツールのLP',
-    default: '小説の執筆ツールのLP',
+    template: "%s | 小説の執筆ツールのLP",
+    default: "小説の執筆ツールのLP",
   },
-  description:
-    '模写修行の小説の執筆ツールのLPをNext.jsで作成したサイトです。',
+  description: "模写修行の小説の執筆ツールのLPをNext.jsで作成したサイトです。",
   openGraph: {
-    title: '小説の執筆ツールのLP',
+    title: "小説の執筆ツールのLP",
     description:
-      '模写修行の小説の執筆ツールのLPをNext.jsで作成したサイトです。',
-    images: ['/ogp.png'],
+      "模写修行の小説の執筆ツールのLPをNext.jsで作成したサイトです。",
+    images: ["/ogp.png"],
   },
   alternates: {
-    canonical: 'https://cwstudio-lp-copy.vercel.app',
+    canonical: "https://cwstudio-lp-copy.vercel.app",
   },
 };
 
@@ -30,8 +29,10 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <Header />
-        {children}
+        <div className="content">
+          <Header />
+          <main>{children}</main>
+        </div>
         <Footer />
       </body>
     </html>
